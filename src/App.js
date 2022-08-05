e7139953850c58ac6b26d204b7eae8dfe3cea2cf
Naveen Kumar M <mnaveen05.04.1999@gmail.com>
2022-08-05 21:57:42 +0530

    🚚 Added Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-
+import 'swiper/css';
+import './assets/boxicons-2.0.7/css/boxicons.min.css'
 import './App.scss';
+
+import { BrowserRouter } from 'react-router-dom'
+
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+
+import AppRoutes from './config/AppRoutes';
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+    <Header/>
+    <AppRoutes/>
+    <Footer/>
+    </BrowserRouter>
   );
 }
 
---
+++ src/config/AppRoutes.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+
+import Catalog from '../pages/Catolog';
+import Detail from '../pages/Detail';
+import Home from '../pages/Home'
+
+
+const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route 
+            path='/:category/search/:keyword' 
+            element={<Catalog/>}
+            />
+            <Route 
+            path='/:category/:id' 
+            element={<Detail/>}
+            />
+            <Route 
+            path='/:category' 
+            element={<Catalog/>}
+            />
+            <Route 
+            path='/' 
+            exact
+            element={<Home/>}
+            />
+        </Routes>
+    )
+}
+
+export default AppRoutes
