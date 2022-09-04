b9c8b51bdfd23b1e3f4cb39e234aefc284cab6ef
Naveen Kumar M <mnaveen05.04.1999@gmail.com>
2022-09-04 14:58:45 +0530

    Added Movie Detail Page,Movie List

--- src/pages/Catolog.jsx
+++ src/pages/Catolog.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
+import { useParams } from 'react-router-dom'
+
+import PageHeader from '../Components/page-header/PageHeader'
+import {category as cate} from '../api/tmdbApi';
+import MovieGrid from '../Components/movie-grid/MovieGrid';
+
+
 const Catolog = () => {
+  const {category}  = useParams()
   return (
-    <div>Catolog</div>
+    <>
+    <PageHeader>
+      {category === cate.movie ? 'Movies':'Tv Series'}
+    </PageHeader>
+    <div className="container">
+      <div className="section mb-3">
+        <MovieGrid category={category}/>
+      </div>
+    </div>
+    </>
   )
 }
 
